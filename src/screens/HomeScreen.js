bf08d1703ed6dcff6cacd647512f2b9dbfccaffc
Kristina Calumpong <kcalumpong@gmail.com>
2020-01-21 22:50:34 -0800

    initial commit of mini projects

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, StyleSheet, View, Button } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+
+  return < View>
+  <Text style={styles.text}>Kristina</Text>
+  < Button 
+      onPress={() => navigation.navigate('Components')}
+      title="Go to Components Demo"/>
+  <Button 
+    title="Go to List Demo"
+    onPress={() => navigation.navigate('List')}
+  />
+  <Button 
+    title="Go to Image Screen Demo"
+    onPress={() => navigation.navigate('Image')}
+  />
+  </View>
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default HomeScreen;
---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
+
+const ImageScreen = () => {
+    return < View>
+    <ImageDetail 
+     title="Forest" 
+     score={9}
+     imageSource={require('../../assets/forest.jpg')}
+    />
+    <ImageDetail 
+     title="Beach"
+     score={7} 
+     imageSource={require('../../assets/beach.jpg')}
+     />
+    <ImageDetail 
+     title="Mountain" 
+     score={10}
+     imageSource={require('../../assets/mountain.jpg')}
+    />
+    </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageScreen;
